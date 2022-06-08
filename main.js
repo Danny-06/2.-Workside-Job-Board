a280f4bb6b93933fca31ae85196bf244323eb921
Xion-14 <xionremenber@gmail.com>
2022-06-08 13:15:05 +0200

    Update

--- main.js
+++ main.js
@@ -38,16 +38,18 @@ window.addEventListener('click', event => {
  * 
  * @param {HTMLElement} container 
  * @param {HTMLTemplateElement} template 
- * @param {{name: string, list: string[]}[]} data 
+ * @param {{name: string, list: string[]}[]} filters 
  */
-function populateAsideFilterUI(container, template, data) {
-  data.forEach(filter => {
+function populateAsideFilterUI(container, template, filters) {
+  filters.forEach(filter => {
     /**
      * @type {DocumentFragment}
      */
     const content = template.content.cloneNode(true)
 
     const filterContainer = content.querySelector('.filter')
+
+    filterContainer.dataset.filterType = filter.name
 
     filterContainer.querySelector('.title').innerHTML = filter.name
 
@@ -61,6 +63,7 @@ function populateAsideFilterUI(container, template, data) {
     filter.list.forEach(item => {
       const filterItem = filterContainer.appendChild(labelCheckbox.cloneNode(true))
 
+      filterItem.dataset.filter = item
       filterItem.querySelector('.label').innerHTML = item
     })
 
@@ -73,8 +76,44 @@ function populateAsideFilterUI(container, template, data) {
  * 
  * @param {HTMLElement} container 
  * @param {HTMLTemplateElement} template 
- * @param {{name: string, icon: string, location: {value: string, ui_value: string}, tags: string[], isPaymentVerified: boolean, info: {name: string, time: number, details: {experience: string, location: string, salary_range: string}, company_overview: string, requirements: string[]}} data 
+ * @param {{name: string, icon: string, location: {value: string, ui_value: string}, tags: string[], isPaymentVerified: boolean, info: {name: string, time: number, details: {experience: string, location: string, salary_range: string}, company_overview: string, requirements: string[]}}[]} jobs 
  */
-function populateJobCardsUI(container, template, data) {
-  
+function populateJobCardsUI(container, template, jobs) {
+  jobs.forEach((job, index) => {
+    /**
+     * @type {DocumentFragment}
+     */
+    const content = template.content.cloneNode(true)
+
+    const jobCard = content.querySelector('.job-card')
+    jobCard.dataset.index = index
+
+    jobCard.querySelector('.top > .logo').src = job.icon
+    jobCard.querySelector('.top > .title > .name').innerHTML = job.name
+    jobCard.querySelector('.top > .title > .location > .text').innerHTML = job.location.ui_value
+
+    const tag = (function() {
+      const tag = jobCard.querySelector('.bottom > .tags > .tag')
+      tag.remove()
+
+      return tag
+    })()
+
+    job.tags.forEach(tagName => {
+      const tagClone = tag.cloneNode(true)
+      tagClone.innerHTML = tagName
+
+      jobCard.querySelector('.bottom > .tags').append(tagClone)
+    })
+
+    if (job.isPaymentVerified) {
+      jobCard.querySelector('.bottom > .payment > .icon').src = 'assets/icons/Checkmark Verified.svg'
+      jobCard.querySelector('.bottom > .payment > .text').innerHTML = 'Payment Verified'
+    } else {
+      jobCard.querySelector('.bottom > .payment > .icon').src = 'assets/icons/Checkmark Not Verified.svg'
+      jobCard.querySelector('.bottom > .payment > .text').innerHTML = 'Payment Unverified'
+    }
+
+    container.append(content)
+  })
 }
